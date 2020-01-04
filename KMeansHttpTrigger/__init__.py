d0997f6a21a152022e12331aa53a0969eacdaf6e
msklyar <michael.sklyar@cyren.com>
2020-01-04 22:18:29 +0200

    Added some parameters handling.

--- KMeansHttpTrigger/__init__.py
+++ KMeansHttpTrigger/__init__.py
@@ -1,7 +1,6 @@
 import logging
 
 import azure.functions as func
-import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 from io import StringIO
@@ -10,38 +9,62 @@ from io import StringIO
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    file_sent = None
-
     try:
+        # mandatory parameters
         file_sent = req.get_body()
+        clusters = int(req.params.get('clusters'))
+        csv_data = file_sent.decode("utf-8")
     except ValueError:
-        pass
+        logging.error("There was an error: %s",  ValueError)
+        return func.HttpResponse(
+            "There was an error: %s" % ValueError,
+            status_code=400
+        )
 
-    csv_data = file_sent.decode("utf-8")
+    # optional parameters
+    col_from = req.params.get('col_from')
+    col_to = req.params.get('col_to')
+    separator = req.params.get('separator', ",")
 
     if file_sent:
-        ret = run_kmeans(csv_data)
+        logging.info("Request data: data_size=%s, #of clusters=%s, col_from=%s, col_to=%s, separator=%s",
+                     len(file_sent),
+                     clusters,
+                     col_from,
+                     col_to,
+                     separator)
+
+        ret = run_kmeans(csv_data, int(clusters), col_from, col_to, separator)
         return func.HttpResponse(str(ret))
     else:
+        logging.error("No data sent in request, returning code 400")
         return func.HttpResponse(
             "Please pass a file in the request body",
             status_code=400
         )
 
-def run_kmeans(csv) -> str:
-    df = pd.read_csv(StringIO(csv), sep=",")
 
-    x = df.iloc[:, [0, 1, 2, 3]].values
-    kmeans3 = KMeans(n_clusters=3)
+def run_kmeans(csv: str, clusters: int, col_from: str, col_to: str, separator: str) -> str:
+    df = pd.read_csv(StringIO(csv), sep=separator)
+
+    # validating col_from and col_to. In caes not set, initialize to 0:LAST_COLUMN
+    if len(col_from) > 0:
+        col_from = int(col_from)
+    else:
+        col_from = 0
+
+    if len(col_to) > 0:
+        col_to = int(col_to)
+    else:
+        col_to = len(df.columns) - 1
+
+    if df.size < 1:
+        raise Exception('Data Frame size is too small: %s')
+
+    x = df.iloc[:, col_from:col_to].values
+    kmeans3 = KMeans(n_clusters=clusters)
     y_kmeans3 = kmeans3.fit_predict(x)
 
     return y_kmeans3
-    # # return df.head(10)
-    # return len(csv)
-
-def prepare_result(x, y_kmeans) -> str:
-
-    # clusters_dict = {}
-    # for index, row in x.iterrows():
 
 
--- Makefile
+++ Makefile
@@ -7,3 +7,6 @@ run-locally:
 
 deploy-to-azure:
 	func azure functionapp publish KMeansAzureFunction
+
+send-sample-request:
+	curl --data-binary "@SampleDataSets/iris.csv" 'http://localhost:7071/api/KMeansHttpTrigger?clusters=3&col_from=0&col_to=3&separator=,'
